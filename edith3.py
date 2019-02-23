24c690544083de1e3ff798830c080ba60c3d6c23
MartinCastroAlvarez <you@example.com>
2019-02-23 04:10:34 -0300

    update

--- edith3.py
+++ edith3.py
@@ -8,47 +8,53 @@ HOME: https://www.martincastroalvarez.com
 DATE: 2019, Feb 21th.
 
 You can execute this script by running this:
->>> cat sample-01.in | python3 edith3.py
+>>> cat sample-01.in | python3 -m cProfile edith3.py
 """
 
 import sys
-import timeit
 
-import logging
+# import timeit
+# import logging
+
+# logger = logging.getLogger(__name__)
+# logger.addHandler(logging.StreamHandler())
+# logger.setLevel(logging.DEBUG)
+# start = timeit.timeit()
 
 UNFIT = 0
-FIT = 1
-ONGOING = 2
+FITTEST = 1
+FIT = 2
 
-MAX_AGES = 12
+MAX_GENERATIONS = 12  # NOTE: 1 <= k <= 11
 DEFAULT_SHORTEST_SURVIVOR = 100000
 IMPOSSIBLE = "IMPOSSIBLE"
 
-start = timeit.timeit()
-
-logger = logging.getLogger(__name__)
-logger.addHandler(logging.StreamHandler())
-# logger.setLevel(logging.DEBUG)
+MAX_STRING_SIZE = 100
+MAX_GENOTYPES = 11
 
 
-def mutate(sequence: list, a: list, b:list, gene: list) -> list:
-
-    logger.debug("------------------------------------------------")
-    logger.debug("%s | %s %s | %s", sequence, a, b, gene)
-    a = a + gene[1]
-    b = b + gene[2]
-
+def get_fitness(a: str, b: str) -> str:
+    """
+    This function returns the fitness of 2 variables
+    in this genetic algorithm.
+    Fitness will increase as 'a' and 'b' are more similar.
+    """
     if a == b:
-        logger.debug("FIT")
-        return FIT, sequence + a, [], []
-
+        return FITTEST
     if a[:len(b)] == b[:len(a)]:
-        logger.debug("ONGOING")
-        return ONGOING, sequence + a[:len(b)], a[len(b):], b[len(a):]
+        return FIT
+    return UNFIT
 
-    logger.debug("UNFIT")
-    return UNFIT, [], [], []
 
+def mutate(phenotype: tuple, genotype: tuple) -> tuple:
+    """
+    This function will combine a phenotype and a genotype
+    into a new phenotype, as if the phenotype mutated
+    with the new genetic code.
+    """
+    a = phenotype[2] + genotype[0]
+    b = phenotype[3] + genotype[1]
+    return get_fitness(a, b), phenotype[1] + a[:len(b)], a[len(b):], b[len(a):]
 
 # The following piece of code reads from stdin
 # and truncates the input by case length..
@@ -56,111 +62,94 @@ output = []
 for line in sys.stdin:
 
     # Reading block of characters from stdin.
-    genes = (
-        next(sys.stdin).replace("\n", "").split()
+    genotypes = (
+        next(sys.stdin).rstrip().split()
         for _ in range(int(line))  # WARNING: May raise ValueError.
     )
-    genes = [
+
+    # By creating a set from the genotypes,
+    # uniqueness is assured.
+    genotypes = {
         (
-            i,
-            list(gene[0]),
-            list(gene[1]),
+            tuple(ord(g) for g in genotype[0][:MAX_STRING_SIZE].lower()),
+            tuple(ord(g) for g in genotype[1][:MAX_STRING_SIZE].lower()),
+            len(genotype[0]),
+            len(genotype[1]),
         )
-        for i, gene in enumerate(genes)
-    ]
+        for genotype in genotypes
+    }
+    # raise Exception(genotypes)
 
     # Detecting systems that might diverge.
     # If all elements on the left or the right
-    # are longer than the other gene, it means
+    # are longer than the other genotype, it means
     # the system will inevitable diverge.
     a_diverges = all(
-        len(gene[1]) > len(gene[2])
-        for gene in genes
+        genotype[2] > genotype[3]
+        for genotype in genotypes
     )
     b_diverges = all(
-        len(gene[2]) > len(gene[1])
-        for gene in genes
+        genotype[2] < genotype[3]
+        for genotype in genotypes
     )
     if a_diverges or b_diverges:
         output.append(IMPOSSIBLE)
         continue
 
     # Generating initial population.
-    chromosomes = (
-        mutate(sequence=[], a=[], b=[], gene=gene)
-        for gene in genes
-        if gene[1][:len(gene[2])] == gene[2][:len(gene[1])]
-        and gene[2][:len(gene[1])] == gene[1][:len(gene[2])]
+    phenotypes = (
+        (FIT, (), (), ()),
     )
-    chromosomes = {
-        hash(str(chromosome)): chromosome
-        for chromosome in chromosomes
-        if chromosome[0] != UNFIT
-    }
 
-    # Catching survivors in the origins.
-    survivors = [
-        chromosome[1]
-        for chromosome in chromosomes.values()
-        if chromosome[0] == FIT
-    ]
-    shortest_survivor = min(
-        len(survivor)
-        for survivor in survivors
-    ) if survivors else DEFAULT_SHORTEST_SURVIVOR
+    # Initializing survivors.
+    survivors = []
+    shortest_survivor = DEFAULT_SHORTEST_SURVIVOR
 
-    # For each age, mutations will be performed
-    # on the chromosomes and only the survivors will be kept.
-    age = 1
-    while True:
-
-        logger.debug("====================================")
-        logger.debug("Age:%s", age)
-        logger.debug("Chromosomes=%s", chromosomes)
-        logger.debug("====================================")
+    # For each generation, mutations will be performed
+    # on the phenotypes and only the survivors will be kept.
+    for generation in range(MAX_GENERATIONS):
 
         # Generating mutations.
-        chromosomes = (
-            mutate(sequence=chromosome[1],
-                   a=chromosome[2],
-                   b=chromosome[3],
-                   gene=gene)
-            for chromosome in chromosomes.values()
-            for gene in genes
-            if chromosome[0] == ONGOING
-            and len(chromosome[1]) < shortest_survivor
+        phenotypes = (
+            mutate(phenotype=phenotype, genotype=genotype)
+            for phenotype in phenotypes
+            for genotype in genotypes
+            if phenotype[0] == FIT
+            and len(phenotype[1]) < shortest_survivor
             and (
-                gene[1][:len(chromosome[3])] == chromosome[3][:len(gene[1])]
-                or gene[2][:len(chromosome[2])] == chromosome[2][:len(gene[2])]
+                genotype[0][:len(phenotype[3])] == phenotype[3][:genotype[2]]
+                or genotype[1][:len(phenotype[2])] == phenotype[2][:genotype[3]]
             )
         )
-        chromosomes = {
-            hash(str(chromosome)): chromosome
-            for chromosome in chromosomes
-            if chromosome[0] != UNFIT
+
+        # Removing unfit and duplicated phenotypes.
+        phenotypes = {
+            phenotype
+            for phenotype in phenotypes 
+            if phenotype[0] != UNFIT
         }
 
-        # Detecting when to stop.
-        if not chromosomes:
+        # Stopping if there are no more mutations.
+        if not phenotypes:
             break
 
         # Detecting fittest chrosomes.
-        survivors.extend(
-            chromosome[1]
-            for chromosome in chromosomes.values()
-            if chromosome[0] == FIT
-        )
-        shortest_survivor = min(
-            len(survivor)
-            for survivor in survivors
-        ) if survivors else DEFAULT_SHORTEST_SURVIVOR
+        survivors.extend(list(filter(lambda phenotype: phenotype[0] == FITTEST,
+                         phenotypes)))
+        shortest_survivor = min(map(lambda survivor: len(survivor),
+                                survivors)) if survivors\
+                                            else DEFAULT_SHORTEST_SURVIVOR
 
     # Detecting the final survivor.
-    survivors = sorted(survivors, key=lambda s: (len(s), s))
-    output.append("".join(survivors[0]) if survivors else IMPOSSIBLE)
+    survivors = sorted(survivors, key=lambda s: (len(s[1]), s[1]))
+    survivor = (
+        chr(s)
+        for s in survivors[0][1]
+    ) if survivors else None
+    output.append("".join(survivor) if survivors else IMPOSSIBLE)
 
 # Printing results to STDOUT.
-print("\n".join(
+sys.stdout.write("\n".join(
     "".join([
         "Case ",
         str(case_number + 1),
@@ -169,4 +158,6 @@ print("\n".join(
     ])
     for case_number, survivor in enumerate(output)
 ))
-logger.debug("Took: %s", timeit.timeit() - start)
+sys.stdout.write("\n")
+
+# logger.debug("Took: %s", timeit.timeit() - start)
